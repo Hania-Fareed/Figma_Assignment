1c5d70aaeb81fe96c2ba9b541f2700e0f804124f
Hania-Fareed <haniafareed7@gmail.com>
2024-10-25 13:19:44 +0500

    figma assignment

---
+++ src/app/components/header.tsx
@@ -0,0 +1,19 @@
+export default function Header(){
+    return(
+        <div>
+            <ul className="headerContainer">
+                <li className="cyber">cyber</li>
+                <li>
+                    <img src="./Search Field.png"/>
+                </li>
+                <li className="home">Home</li>
+                <li >About</li>
+                <li >Contact Us</li>
+                <li >Blog</li>
+                <li className="icons">
+                    <img src="./Icons.png"/>
+                </li>
+            </ul>
+        </div>
+    )
+}
